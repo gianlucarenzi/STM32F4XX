2ab0cfb417895422b882c02ae0145cc138408380
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-11-16 21:23:03 +0100

    Updated

--- STM32F401/STM32_HID_Bootloader/flash.sh
+++ STM32F401/STM32_HID_Bootloader/flash.sh
@@ -9,4 +9,4 @@ if [ ! -f ${EXEC} ]; then
 	exit 1
 fi
 openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; program ${EXEC} verify reset; shutdown"
+-c "init; targets; reset init; wait_halt; poll; program ${EXEC} verify; reset run; shutdown"
